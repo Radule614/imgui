0ff1edf975ec5d110eedf5841d825d23f9cf0fee
Rade Stojanovic <stojanovicrade614@gmail.com>
2026-03-03 23:35:20 +0100

    fix premake, remove gh actions

--- premake5.lua
+++ premake5.lua
@@ -1,34 +1,43 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+    staticruntime "On"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-		"imconfig.h",
-		"imgui.h",
 		"imgui.cpp",
 		"imgui_draw.cpp",
-		"imgui_internal.h",
 		"imgui_widgets.cpp",
+		"imgui_demo.cpp",
+		"imgui.h",
+		"imconfig.h",
+		"imgui_internal.h",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_tables.cpp",
+		"backends/imgui_impl_glfw.cpp",
+		"backends/imgui_impl_opengl3.cpp"
+	}
+
+    includedirs
+	{
+        ".",
+        "../glfw/include"
 	}
 
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "On"
+        defines { "_CRT_SECURE_NO_WARNINGS" }
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "On"
 
 	filter "configurations:Debug"
 		runtime "Debug"
